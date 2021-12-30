b2d01bfc762704277ee46fd983483b8118c4303c
louannem <84766775+louannem@users.noreply.github.com>
2021-12-30 13:51:37 +0100

    Ajout des fonctions de open/close sur collapse

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,16 +1,40 @@
 import React from "react";
 import "../../utils/styles/Collapse.css"
+import ChevronUp from "../../assets/chevron-up.svg"
+import ChevronDown from "../../assets/chevron-down.svg"
 
 class Collapse extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            showCollapse : false,
+        }
+        this.toggleCollapse = this.toggleCollapse.bind(this);
+        console.log(this.props, this.state)
+    }
+
+    toggleCollapse(){
+        const show = this.state.showCollapse
+        this.setState({showCollapse: !show});
+    }
+
     render() {
         return(
             <div className="kasa-collapse">
                 <div className="collpase-header">
-                    {this.props.title}
+                    <span>{this.props.title}</span>
+                    {this.state.showCollapse ?
+                    <img src={ChevronUp} alt="Dérouler" onClick={() => this.toggleCollapse()}/>
+                    :
+                    <img src={ChevronDown} alt="Dérouler" onClick={() => this.toggleCollapse()}/> 
+                    }
+                    
                 </div>
-                <div className="collapse-container">
-                    {this.props.content}
-                </div>
+                {this.state.showCollapse &&
+                    <div className="collapse-container">
+                        <span>{this.props.content}</span>
+                    </div>
+                }
             </div>
         )
     }
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -11,10 +11,10 @@ class About extends React.Component {
                 <Banner title="" img={aboutBanner} className="about-banner"/>
 
                 <div className="about-content">
-                    <Collapse title="Fiabilité" content="Lorem ipsum"/>
-                    <Collapse title="Respect" content="Lorem ipsum"/>
-                    <Collapse title="Service" content="Lorem ipsum"/>
-                    <Collapse title="Responsabilité" content="Lorem ipsum"/>
+                    <Collapse title="Fiabilité" content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."/>
+                    <Collapse title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
+                    <Collapse title="Service" content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."/>
+                    <Collapse title="Responsabilité" content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
                 </div>
             </div>
         )
