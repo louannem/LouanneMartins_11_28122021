438df06b162d722992032c8da267a43f16803668
louannem <84766775+louannem@users.noreply.github.com>
2022-01-03 21:56:33 +0100

    Implémentation des données sur l'accueil

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,22 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import "../../utils/styles/GlobalStyles.css"
 import Banner from "../../components/Banner"
 import Card from "../../components/Card"
 import homeBanner from "../../assets/home-banner.jpg"
+import { data } from "../../data/logements"
 
 class Home extends React.Component {
+
     render() {
         return(
             <div className="kasa-home">
                 <Banner title="Chez vous, partout et ailleurs" img={homeBanner} />
 
                 <div className="location-grid">
-                    <Card name="Titre de la location" />
-                    <Card name="Titre de la location" />
-                    <Card name="Titre de la location" />
-                    <Card name="Titre de la location" />
-                    <Card name="Titre de la location" />
-                    <Card name="Titre de la location" />
+                    {data.map((location) => (
+                       <Link key={location.id} to={`/location/${location.id}`}><Card name={location.title} /></Link>
+                    ))}
                 </div>
             </div>
         )
